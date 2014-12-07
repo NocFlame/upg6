0f09815136414a75036e4c996de9ec022db748f7
NocFlame <aad@nocflame.se>
2014-12-08 00:27:15 +0100

    Heltals klassen gjord

--- AndreasCpp/AndreasCpp.cpp
+++ AndreasCpp/AndreasCpp.cpp
@@ -2,10 +2,18 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+#include "Heltal.h"
 
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	Heltal htal(5);
+	Heltal tal(2);
+	cout << "talen " << htal.get() << " & " << tal.get() << " nice" << std::endl;
+	(htal > tal) ? cout << "true" << endl : cout << "false" << endl;
+	cin.get();
 	return 0;
 }
-
